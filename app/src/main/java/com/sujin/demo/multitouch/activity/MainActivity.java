5b566995390654ed908025a560ca12facd3addaa
sujin <sujin.v2px@gmail.com>
2017-07-29 15:43:21 +0545

    Initial commit

---
+++ app/src/main/java/com/sujin/demo/multitouch/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sujin.demo.multitouch.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sujin.demo.multitouch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
